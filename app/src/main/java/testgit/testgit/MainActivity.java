e48eb2d2ef440fe5b6d795378eda01b7961cb360
淡蓝的天空 <1982582704@qq.com>
2016-11-14 15:24:02 +0800

    地址

--- app/src/main/java/testgit/testgit/MainActivity.java
+++ app/src/main/java/testgit/testgit/MainActivity.java
@@ -2,7 +2,12 @@ package testgit.testgit;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+/** 
+ * 作用:Git使用
+ * autour: Sh
+ * date: 2016/11/14
+ * 地址：http://www.jianshu.com/p/2f4a2358b0f4
+ */
 public class MainActivity extends Activity {
 
     @Override
